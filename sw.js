14f1a9126b189b1222911a38596459673fc2ea23
gunterstruck <224373224+gunterstruck@users.noreply.github.com>
2025-12-02 16:13:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 const REPO_PATH = '/vCard/';
 
 // Cache version - increment to force cache update
-const CORE_CACHE_NAME = 'vcard-core-v06';
+const CORE_CACHE_NAME = 'vcard-core-v10';
 
 // Core Assets for offline availability
 const CORE_ASSETS = [
@@ -181,4 +181,5 @@ self.addEventListener('message', (event) => {
         self.skipWaiting();
     }
 });
+
 
